f25001179f9e20b49b8c55d6eafd09cb75ba4842
danielmorganro-beep <danielmorganro@gmail.com>
2026-09-03 10:37:51 -0500

    Create User model with relationships and attributes

---
+++ MODELOS CON RELACIONES/User.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role_id',
+        'phone',
+        'document',
+        'document_type',
+        'status',
+        'avatar',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function doctor()
+    {
+        return $this->hasOne(Doctor::class);
+    }
+
+    public function notifications()
+    {
+        return $this->hasMany(Notification::class);
+    }
+
+    public function auditLogs()
+    {
+        return $this->hasMany(AuditLog::class);
+    }
+
+    public function medicationMovements()
+    {
+        return $this->hasMany(MedicationMovement::class);
+    }
+
+    public function hospitalizations()
+    {
+        return $this->hasMany(Hospitalization::class, 'admitted_by');
+    }
+
+    public function vitalSigns()
+    {
+        return $this->hasMany(VitalSign::class, 'recorded_by');
+    }
+
+    public function procedures()
+    {
+        return $this->hasMany(Procedure::class, 'performed_by');
+    }
+
+    public function laboratoryResultsUploaded()
+    {
+        return $this->hasMany(LaboratoryResult::class, 'uploaded_by');
+    }
+
+    public function laboratoryResultsApproved()
+    {
+        return $this->hasMany(LaboratoryResult::class, 'approved_by');
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class, 'registered_by');
+    }
+}
